89b572776f197d22e46255417243577ad63c0e0a
Hasan MD Atik <akhasannaeem@gmail.com>
2025-12-05 17:18:05 +0600

    trying to fix path related issues

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router";
-import RootLayout from "../layouts/RootLayout";
+import RootLayout from "../Layouts/RootLayout";
 import Contacts from "../pages/Contacts";
 import Works from "../pages/Works";
 import About from "../pages/About";
