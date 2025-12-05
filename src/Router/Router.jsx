c2064a0e9b6eaa3e53efafc59193a120da31c4fb
Hasan MD Atik <akhasannaeem@gmail.com>
2025-12-05 12:17:29 +0600

    trying fix the routing error

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,9 +1,9 @@
 import { createBrowserRouter } from "react-router";
 import RootLayout from "../Layouts/RootLayout";
-import Home from "../pages/Home";
-import Works from "../pages/Works";
-import About from "../pages/About";
 import Contacts from "../pages/Contacts";
+import Works from "../Pages/Works";
+import About from "../Pages/About";
+import Home from "../pages/Home";
 
 const router = createBrowserRouter([
   {
