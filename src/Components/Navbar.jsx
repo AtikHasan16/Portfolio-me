fafe1bcb474f14b59af343bfa3d512b1c15a17fc
Hasan MD Atik <akhasannaeem@gmail.com>
2025-12-05 09:26:17 +0600

    added favicon

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -84,7 +84,7 @@ const Navbar = () => {
             </div>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-1 p-2 shadow bg-base-100 rounded-box w-52 text-base-content"
+              className="menu  dropdown-content mt-3 z-1 p-2 shadow bg-base-100 rounded-box w-52 text-base-content"
             >
               {navLinks.map((link) => (
                 <li key={link.name}>
--- src/Layouts/RootLayout.jsx
+++ src/Layouts/RootLayout.jsx
@@ -4,7 +4,7 @@ import Navbar from "../components/Navbar";
 
 const RootLayout = () => {
   return (
-    <div>
+    <div className="container mx-auto">
       <header>
         <Navbar />
       </header>
